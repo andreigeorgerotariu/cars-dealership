ac03dffb8a2f593643dad5cce0081a1bf4316446
lpandrew <lp_andrew@yahoo.com>
2023-05-27 18:09:34 +0300

    implemented first and last name validations for Customer

--- src/main/java/com/carsdealership/models/dtos/CustomerDTO.java
+++ src/main/java/com/carsdealership/models/dtos/CustomerDTO.java
@@ -1,6 +1,7 @@
 package com.carsdealership.models.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 import lombok.ToString;
@@ -15,8 +16,10 @@ public class CustomerDTO implements Serializable {
 
     private Long id;
     @NotBlank(message = "first name must not be blank")
+    @Size(min = 2, max = 20, message = "first name must contain between 2 and 20 characters" )
     private String firstName;
     @NotBlank(message = "last name must not be blank")
+    @Size(min = 2, max = 20, message = "first name must contain between 2 and 20 characters" )
     private String lastName;
     @NotBlank(message = "email must not be blank")
     private String email;
