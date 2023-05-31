7db5237a8039e6ad2f490cfad7c722385b62b3b7
lpandrew <lp_andrew@yahoo.com>
2023-05-31 12:32:12 +0300

    Exception handler implementation

--- src/main/java/com/carsdealership/exceptions/CarNotFoundException.java
+++ src/main/java/com/carsdealership/exceptions/CarNotFoundException.java
@@ -1,6 +1,8 @@
 package com.carsdealership.exceptions;
 
-public class CarNotFoundException extends IllegalArgumentException{
+public class CarNotFoundException extends RuntimeException {
 
-    public CarNotFoundException(String message) { super(message);}
+    public CarNotFoundException(String message) {
+        super(message);
+    }
 }
