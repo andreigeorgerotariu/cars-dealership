1c336b3db314d7471cb8a48605a6b839c40dff74
lpandrew <lp_andrew@yahoo.com>
2023-06-01 16:17:17 +0300

    Improved TestCreateCustomerShouldPass to verify that the customer is saved in the repository

--- src/test/java/com/carsdealership/integrationTesting/CustomersServiceIntegrationTest.java
+++ src/test/java/com/carsdealership/integrationTesting/CustomersServiceIntegrationTest.java
@@ -1,18 +1,24 @@
 package com.carsdealership.integrationTesting;
 
 import com.carsdealership.models.dtos.CustomerDTO;
+import com.carsdealership.models.entities.Customer;
+import com.carsdealership.repositories.CustomerRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Slf4j
@@ -23,29 +29,46 @@ class CustomersServiceIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    void testCreateCustomerShouldPass() throws Exception {
-        CustomerDTO customerDTO = CustomerDTO.builder()
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private CustomerDTO customerDTO;
+
+    @MockBean
+    private CustomerRepository customerRepository;
+
+    @BeforeEach
+    void setup() {
+        customerDTO = CustomerDTO.builder()
                 .firstName("Maria")
                 .lastName("Ioana")
-                .email("tuesticampioana@mail.com")
+                .email("tuesticampioana@example.com")
                 .build();
-        mockMvc.perform(post("/api/customers")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectToString(customerDTO)))
-                .andExpect(status().is(200))
-                .andDo(print());
     }
 
-    private String objectToString(Object object) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            objectMapper.findAndRegisterModules();
-            return objectMapper.writeValueAsString(object);
+    @Test
+    void TestCreateCustomerShouldPass() throws Exception {
+        Customer savedCustomer = objectMapper.convertValue(customerDTO, Customer.class);
+        savedCustomer.setId(1L);
+        when(customerRepository.save(any(Customer.class))).thenReturn(savedCustomer);
 
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/customers")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectToString(customerDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName").value(customerDTO.getFirstName()))
+                .andExpect(jsonPath("$.lastName").value(customerDTO.getLastName()))
+                .andExpect(jsonPath("$.email").value(customerDTO.getEmail()));
+
+        verify(customerRepository, times(1)).save(any(Customer.class));
+    }
+    private String objectToString(Object object) {
+        try {
+            return objectMapper.writeValueAsString(object);
         } catch (JsonProcessingException exception) {
-            log.error(exception.getMessage());
+            log.error("Error occurred during object serialization: {}", exception.getMessage());
         }
         return null;
     }
+
 }
