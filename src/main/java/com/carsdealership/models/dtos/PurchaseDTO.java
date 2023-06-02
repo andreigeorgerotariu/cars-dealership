e93f02954c88e0c05c18ba14efeb8f16183b387b
lpandrew <lp_andrew@yahoo.com>
2023-06-02 10:28:15 +0300

    DTO fields and validations update

--- src/main/java/com/carsdealership/models/dtos/PurchaseDTO.java
+++ src/main/java/com/carsdealership/models/dtos/PurchaseDTO.java
@@ -1,6 +1,8 @@
 package com.carsdealership.models.dtos;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Data;
 import lombok.ToString;
@@ -16,4 +18,8 @@ public class PurchaseDTO implements Serializable {
     private long id;
     @NotNull(message = "Purchase date must not be blank")
     private LocalDateTime purchaseDate;
+    @Positive(message = "Total price must be a positive value")
+    private double totalPrice;
+    @Pattern(regexp = "^(cash|card)$", message = "Payment method must be either 'cash' or 'card'")
+    private String paymentMethod;
 }
